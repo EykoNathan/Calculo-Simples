260a58cdb9cb87d23a46bf690e0b1ca089e22280
EykoNathan <90914758+EykoNathan@users.noreply.github.com>
2021-10-06 08:39:56 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.querySelector("#calcular").addEventListener("click", function() {
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+    if(valorA.length > 0 && valorB.length > 0){
+        alert(parseInt(valorA) + parseInt(valorB));
+    }
+    else{
+        alert("Digite um valor para o cálculo!");
+    }
+})
